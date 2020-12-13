c50f063b5143702f0ad49663975bf3180dd1a463
David Deloso <daviddeloso2022@u.northwestern.edu>
2020-12-12 19:23:26 -0600

    initial commit for BetterYesterday

---
+++ hangover/BetterYesterday/main.js
@@ -0,0 +1,133 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#content1", {
+  scrollTrigger: {
+    trigger: "#content1",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+
+gsap.to("#content2", {
+  scrollTrigger: {
+    trigger: "#content2",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+
+gsap.to("#content3", {
+  scrollTrigger: {
+    trigger: "#content3",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content4", {
+  scrollTrigger: {
+    trigger: "#content4",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content5", {
+  scrollTrigger: {
+    trigger: "#content5",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content6", {
+  scrollTrigger: {
+    trigger: "#content6",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content7", {
+  scrollTrigger: {
+    trigger: "#content7",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content8", {
+  scrollTrigger: {
+    trigger: "#content8",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content9", {
+  scrollTrigger: {
+    trigger: "#content9",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content10", {
+  scrollTrigger: {
+    trigger: "#content10",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content11", {
+  scrollTrigger: {
+    trigger: "#content11",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content12", {
+  scrollTrigger: {
+    trigger: "#content12",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content13", {
+  scrollTrigger: {
+    trigger: "#content13",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content14", {
+  scrollTrigger: {
+    trigger: "#content14",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+
+gsap.to("#content15", {
+  scrollTrigger: {
+    trigger: "#content15",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 1.5,
+});
+gsap.to("#content16", {
+  scrollTrigger: {
+    trigger: "#content16",
+    toggleActions: "resume pause resume pause",
+  },
+  opacity: 1,
+  duration: 2,
+});
