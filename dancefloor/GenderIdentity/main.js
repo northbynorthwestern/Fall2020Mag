f90024a263dfb314638a6416b85fb0fbc50f4259
David Deloso <daviddeloso2022@u.northwestern.edu>
2020-12-16 21:44:47 -0600

    initial commit for Gender Identity

---
+++ dancefloor/GenderIdentity/main.js
@@ -0,0 +1,24 @@
+gsap.to(".circle", {
+  opacity: 1,
+  ease: "circ.in",
+  duration: 2,
+});
+
+gsap.to(".greencircle", {
+  scrollTrigger: ".greencircle", // selector or element
+  opacity: 1,
+  ease: "circ.in",
+  duration: 2,
+});
+
+gsap.to(".browncircle", {
+  scrollTrigger: ".browncircle", // selector or element
+  opacity: 1,
+  duration: 2,
+});
+
+gsap.to(".pinkcircle", {
+  scrollTrigger: ".pinkcircle", // selector or element
+  opacity: 1,
+  duration: 2,
+});
