cdb7505f9d7b6bee139ec0e3059288d94f34aaa8
David Deloso <daviddeloso2022@u.northwestern.edu>
2020-12-17 22:15:53 -0600

    added images

---
+++ hangover/KamaSutra/main.js
@@ -0,0 +1,17 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.from("#dildo1", 
+{scrollTrigger:{
+    trigger: "#dildo1"
+}, opacity: 0, duration: 2}, 1);
+gsap.from("#buttplug", 
+{scrollTrigger:{
+    trigger: "#buttplug"
+}, opacity: 0, duration: 2}, 1);
+gsap.from("#dildo2", 
+{scrollTrigger:{
+    trigger: "#dildo2"
+}, opacity: 0, duration: 2}, 1);
+gsap.from("#vibrator", 
+{scrollTrigger:{
+    trigger: "#vibrator"
+}, opacity: 0, duration: 2}, 1);
