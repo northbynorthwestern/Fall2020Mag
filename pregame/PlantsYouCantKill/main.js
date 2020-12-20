efb8c996719dc2ad4c22f8517f2a9ef9dcb1b8f9
David Deloso <daviddeloso2022@u.northwestern.edu>
2020-12-19 18:50:38 -0600

    Initial commit for leaves that last

---
+++ pregame/PlantsYouCantKill/main.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".moneytree", {
+  scrollTrigger: ".moneytree",
+  opacity: 1,
+  duration: 2,
+});
+gsap.to(".devilsivy", {
+  scrollTrigger: ".devilsivy",
+  opacity: 1,
+  duration: 2,
+});
+gsap.to(".snakeplant", {
+  scrollTrigger: ".snakeplant",
+  opacity: 1,
+  duration: 2,
+});
+
+gsap.to(".peacelily", {
+  scrollTrigger: ".peacelily",
+  opacity: 1,
+  duration: 2,
+});
